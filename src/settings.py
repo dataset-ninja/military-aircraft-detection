9c89a907da8a979cbd81a437a70397d8107417f2
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-07 16:20:58 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -43,7 +43,7 @@ GITHUB_URL: str = "https://github.com/dataset-ninja/military-aircraft-detection"
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = ["https://www.kaggle.com/datasets/a2015003713/militaryaircraftdetectiondataset"]
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://www.kaggle.com/datasets/a2015003713/militaryaircraftdetectiondataset"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -57,6 +57,7 @@ REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
 AUTHORS: Optional[List[str]] = ["T Nakamura"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["https://www.kaggle.com/a2015003713"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
@@ -110,6 +111,7 @@ def get_settings():
     settings["repository"] = REPOSITORY
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
+    settings["authors_contacts"] = AUTHORS_CONTACTS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
